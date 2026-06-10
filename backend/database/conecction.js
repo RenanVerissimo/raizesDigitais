4ef65560193c97937174197d7eafd7bfca6cfc5e
RenanVerissimo <rv.sv@hotmail.com>
2026-06-10 00:21:14 -0300

    add linha no arq conecction2

--- backend/database/conecction.js
+++ backend/database/conecction.js
@@ -12,6 +12,7 @@ const pool = mysql.createPool({
 
 module.exports = pool; */
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
