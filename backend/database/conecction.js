113e330dcdb239315d38a33c47455b794c2bf858
RenanVerissimo <rv.sv@hotmail.com>
2026-06-09 23:41:26 -0300

    fix mysql railway port

--- backend/database/conecction.js
+++ backend/database/conecction.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+/* const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
@@ -6,6 +6,19 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "12340",
     database: process.env.DB_NAME || "raizes_digitais",
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
+module.exports = pool; */
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "12340",
+    database: process.env.DB_NAME || "raizes_digitais",
+    port: process.env.DB_PORT || 3306,
+
     waitForConnections: true,
     connectionLimit: 10,
 });
