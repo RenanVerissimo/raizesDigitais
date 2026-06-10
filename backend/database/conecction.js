96350253d9cb8b6e3c749d8a3beef143fe8ad356
RenanVerissimo <rv.sv@hotmail.com>
2026-06-09 23:47:31 -0300

    teste

--- backend/database/conecction.js
+++ backend/database/conecction.js
@@ -23,4 +23,11 @@ const pool = mysql.createPool({
     connectionLimit: 10,
 });
 
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+});
+
 module.exports = pool;
