e96267c79b1fb6d49794b54fddee6817cbaff3e4
RenanVerissimo <rv.sv@hotmail.com>
2026-06-18 19:49:56 -0300

    remocao de valores de conexao do bd locais

--- backend/database/conecction.js
+++ backend/database/conecction.js
@@ -14,21 +14,15 @@ module.exports = pool; */
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "12340",
-    database: process.env.DB_NAME || "raizes_digitais",
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 
     waitForConnections: true,
     connectionLimit: 10,
 });
 
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME,
-});
 
 module.exports = pool;
